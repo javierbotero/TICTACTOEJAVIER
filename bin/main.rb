e7bcce7ca9bed90807ac8bb00dadbc944407ac93
Javier Botero <javierbotero10271@gmail.com>
2020-03-29 19:08:05 -0500

    Start to add logic to the game

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 require './lib/player'
+require './lib/board'
 
 puts '--||---||---- ** This is the tick tack toe game ** ----||--||---'
 
@@ -8,9 +9,27 @@ player_x = Player.new(gets.chomp, 'X')
 puts 'Write a name for the player 2, which will be (O)'
 player_o = Player.new(gets.chomp, 'O')
 player_o.verify(player_x)
-puts "The main idea is to complete an entire line, \n could be horintally, vertically, \n or an inclined line. Each pleayer \n one at a time choose a field with \n a given number show in the board below"
+print "The main idea is to complete an entire line, \n could be horizontally, vertically, \n or an inclined line. Each pleayer \n one at a time choose a field with \n a given number show in the board below"
+9.times { |x|
+  if x % 3 == 0
+    print "\n-------------\n|"
+    print " #{x + 1} |"
+  else
+    print " #{x + 1} |"
+    puts "\n-------------" if x == 8
+  end
+}
 board = Board.new(player_x, player_o)
 board.display_board
-loop {
-   "#{player_x.name} select" 
-}
+switcher = true
+counter = 1
+while switcher
+  if counter.odd?
+    board.mark (player_x.name, player_x.sign)
+  else
+    board.mark (player_o.name, player_o.sign)
+  end
+  board.evaluation
+  board.display_board
+  counter += 1
+end
--- lib/board.rb
+++ lib/board.rb
@@ -1,26 +1,55 @@
 class Board
-  attr_accessor :board, :score1, :score2
+  attr_accessor :board, :scoreX, :scoreO
     
   def initialize(player1, player2)
     @board = {
-      one: 1,
-      two: 2,
-      three: 3,
-      four: 4,
-      five: 5,
-      six: 6,
-      seven: 7,
-      eight: 8,
-      nine: 9
+      1 => 1,
+      2 => 2,
+      3 => 3,
+      4 => 4,
+      5 => 5,
+      6 => 6,
+      7 => 7,
+      8 => 8,
+      9 => 9
     }
-    @score1 = 0
-    @score2 = 0
+    @scoreX = 0
+    @scoreO = 0
   end
 
-  def display_board
-    self.board.each do |position, value|
-      puts "------------------"
-      puts value.is_a? Integer? "|#{value}|" : "| |"
+  def display_board    
+    self.board.each do |key, value|
+      print "\n-------------\n|" if [1, 4, 7].include?(key)
+      if value.is_a? String
+        print " #{value} |"
+      else
+        print "   |"
+      end
+      print "\n-------------\n" if key == 9
+    end          
+  end
+
+  def mark (name, sign)
+    loop do
+      puts "player #{name} select a number"
+      selection = gets.chomp
+      if selection > 0 && selection < 10
+        self.board[selection] = sign if self.board[selection].is_a? Integer
+        break
+      else
+        puts "please write a number between and including 1 and 9"
+      end
     end
+  end
+
+  def evaluation
+    i = 1
+    while i > 7    
+      return self.board[i] if self.board[i] == self.board[i + 1] && self.board[i + 1] == self.board[i + 2]
+      return self.board[i] if self.board[i] == self.board[i + 3] && self.board[i + 3] == self.board[i + 6]      
+      i += 3
+    end
+    return self.board[5] if self.board[5] == self.board[1] && self.board[5] == self.board[9]
+    return self.board[5] if self.board[5] == self.board[3] && self.board[5] == self.board[7]
   end
 end
